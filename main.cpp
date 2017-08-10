0f04f7b202db8c983b5ee6a265e7239fdde4b13b
Benjamin J. Anderson <andeb2804@gmail.com>
2017-08-09 21:00:54 -0500

    update readme

--- main.cpp
+++ main.cpp
@@ -6,17 +6,15 @@ int main( const int argc, const char **argv )
 {
   if ( argc == 2 )
   {
-    Frontend::Scanner *scanner;
-    Frontend::Parser *parser;
     const char * const fileName = argv[1];
     std::ifstream inFile( fileName );
 
     if ( !inFile.good() ) { return 2; }
 
-    scanner = new Frontend::Scanner(&inFile);
-    parser = new Frontend::Parser(*scanner);
+    Frontend::Scanner scanner( &inFile );
+    Frontend::Parser parser( scanner );
 
-    if ( parser->parse() != 0 ) { return 3; }
+    if ( parser.parse() != 0 ) { return 3; }
 
     return 0;
   }
